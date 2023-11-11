bbb27fbaf267ac6415266ab23467a0c54fae041b
JonsonNieve <santi9119@outlook.com>
2023-11-11 16:43:14 -0300

    Initial Commit

---
+++ src/main/java/net/jonsonnieve/ben10/Ben10ModClient.java
@@ -0,0 +1,11 @@
+package net.jonsonnieve.ben10;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Ben10ModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/jonsonnieve/ben10/Ben10ModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jonsonnieve.ben10;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Ben10ModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
